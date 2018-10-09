48ff85be78ecb780238429d7536796bbc7022957
Popcornblaster <lucinablaster@gmail.com>
2018-10-09 11:57:25 -0600

    Initial Commit

---
+++ FirstIOSProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstIOSProject
+//
+//  Created by Baugh, Tyler on 10/9/18.
+//  Copyright © 2018 Baugh, Tyler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
