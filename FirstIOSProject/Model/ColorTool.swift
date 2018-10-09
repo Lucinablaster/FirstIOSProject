cd0069ac4bf6b88e41b215223000406dcd24992f
Popcornblaster <lucinablaster@gmail.com>
2018-10-09 12:20:39 -0600

    Added model group and renamed files

---
+++ FirstIOSProject/Model/ColorTool.swift
@@ -0,0 +1,15 @@
+//
+//  ColorTool.swift
+//  FirstIOSProject
+//
+//  Created by Baugh, Tyler on 10/9/18.
+//  Copyright © 2018 Baugh, Tyler. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ColorTool
+{
+
+}
